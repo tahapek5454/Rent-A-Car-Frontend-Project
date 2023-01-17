f7cc9bb24cce78082eb10992a6009ccb7d10eadc
tahapek5454 <tahapek5454@gmail.com>
2023-01-17 17:06:00 +0300

    add rent car operation from cart

--- src/app/component/pay/pay.component.ts
+++ src/app/component/pay/pay.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CartItem } from 'src/app/models/cartItem/cartItem';
+import { Rent } from 'src/app/models/rental/rent';
 
 import { CartService } from 'src/app/services/cart/cart.service';
+import { RentalService } from 'src/app/services/rental/rental.service';
 
 @Component({
   selector: 'app-pay',
@@ -13,15 +15,86 @@ export class PayComponent implements OnInit{
 
   cartItems:CartItem[]=[]
   total:number=0
+  rent:Rent[]=[]
  
 
-  constructor(private cartService:CartService, private taostrService:ToastrService){}
+  constructor(private cartService:CartService,
+              private toastrService:ToastrService,
+              private rentalService:RentalService){}
 
   ngOnInit(): void {
     this.getCart()
     this.calculate()
+    this.getRent()
     
   }
+
+  sleep() {
+    return new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
+  getRent(){
+    this.cartItems.forEach(element => {
+      let myrent = new Rent()
+      myrent.customerId = 3
+      myrent.carId = element.carDetail.carId
+      
+      this.rent.push(myrent)
+             
+    });
+  }
+
+  async add(){
+
+
+        console.log(this.rent)
+    
+        for (let a = 0 ; a< this.rent.length; a++){
+
+        let element = this.rent[a]
+
+        console.log(JSON.stringify(element))
+
+        this.rentalService.addRental(element).subscribe(response =>{
+        console.log(response)
+        this.toastrService.success(response.message,"Basarili")
+          
+        },(responseError)=>{  // Business Rules Not exception but bad Request handler
+            console.log(responseError)
+
+            if(Object.keys(responseError.error).length == 2){
+
+            this.toastrService.error(responseError.error.message,"Dogrulama Hatasi")
+
+
+            }
+            else if(responseError.error.Errors.length > 0){ //Validation Exception + BadRequest
+    
+              for (let index = 0; index < responseError.error.Errors.length; index++) {
+                let element = responseError.error.Errors[index];
+    
+                this.toastrService.error(element.ErrorMessage,"Dogrulama Hatasi")
+                
+              }
+              
+    
+            }
+  
+        
+        
+      })
+
+
+      console.log("selam")
+      await this.sleep()
+      
+      
+      
+
+  }
+}
+
+
 
 
   getCart(){
--- src/app/services/rental/rental.service.ts
+++ src/app/services/rental/rental.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
 import { Rent } from 'src/app/models/rental/rent';
 import { Rental } from 'src/app/models/rental/rental';
+import { ResponseModel } from 'src/app/models/responseModel';
 
 
 @Injectable({
@@ -16,6 +17,18 @@ export class RentalService {
 
   getRentalDetail():Observable<ListResponseModel<Rental>>{
     return this.httpClient.get<ListResponseModel<Rental>>(this.apiUrl)
+  }
+
+
+
+
+  addRental(rent:any):Observable<ResponseModel>{
+    let newApiUrl = "https://localhost:44379/api/Rentals/AddRental"
+
+    const headers = { 'content-type': 'application/json'}  
+    const body=JSON.stringify(rent);
+    return this.httpClient.post<ResponseModel>(newApiUrl,  body,{'headers':headers})
+
   }
 
   
